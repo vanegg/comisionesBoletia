e77ef28c463126ec212b641339e8335536a16ff3
Vanessa <van.gg@live.com.mx>
2017-01-26 17:10:42 -0600

    Fix create commission by event and user

--- app/controllers/commission_controller.rb
+++ app/controllers/commission_controller.rb
@@ -7,9 +7,11 @@ class CommissionController < ApplicationController
   end
 
   def create
-    @commission = Commission.new(card: params[:commission][:card],
-    							 										deposit: params[:commission][:deposit]
-    																 )
+    puts '+' * 30
+    puts 'TIPO:' + params[:commission][:type]
+    puts @commission = Commission.new(card: params[:commission][:card],
+                                      deposit: params[:commission][:deposit]
+                                     )
     case params[:commission][:type] 
     when "0"
     	@commission.booking_id = params[:commission][:id]
@@ -18,21 +20,25 @@ class CommissionController < ApplicationController
     	if Event.exists?(params[:commission][:id])
     		event = Event.find(params[:commission][:id])
     		event.hasCommission = true
+        event.save
+        puts "Cambiando has commission de event"
     	end
     else
     	@commission.user_id = params[:commission][:id]
     	if User.exists?(params[:commission][:id])
     		user = User.find(params[:commission][:id])
     		user.hasCommission = true
+        user.save
+        puts "Cambiando has commission de user"
     	end
     end
-
+    puts @commission.save
     if @commission.save
       flash.now[:success] = "Commission successfully saved"
     else
       flash.now[:error] = "Commission not saved in database"
     end
-    redirect_to(:back)
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
