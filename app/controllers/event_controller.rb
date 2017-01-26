973db30e7281357eeab6710310ded9a7faaa7185
Vanessa <van.gg@live.com.mx>
2017-01-25 21:54:34 -0600

    Find event

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -1,4 +1,5 @@
 class EventController < ApplicationController
+
 	def show
 		if params[:event_id] != nil && params[:event_id] != ""
 			id = params[:event_id]
@@ -6,15 +7,10 @@ class EventController < ApplicationController
 				@event = Event.find(id)
 				@status = true
 
-				respond_to do |format|
-      		format.js { flash.now[:success] = "¡Evento encontrado!" }
-    		end
+      	#format.js { flash.now[:success] = "¡Evento encontrado!" }
 			else
 				@status = false
-
-				respond_to do |format|
-      		format.js { flash.now[:danger]	= "¡Evento no encontrado" }
-    		end
+      	#format.js { flash.now[:danger]	= "¡Evento no encontrado" }
 			end
 		end
 	end
