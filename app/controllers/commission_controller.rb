48ed8e1c6bfea230325a77fb9afd224ed21abbe8
Vanessa <van.gg@live.com.mx>
2017-01-26 12:19:38 -0600

    Save commission by event

--- app/controllers/commission_controller.rb
+++ app/controllers/commission_controller.rb
@@ -7,14 +7,28 @@ class CommissionController < ApplicationController
   end
 
   def create
-    @commission = Commission.new()
-    puts "creating commission"
-    # current_user.questions << @question
-    # if @question.save
-    #   flash[:success] = "Question successfully saved"
-    # else
-    #   flash[:error] = "Question not saved in database"
-    # end
-    # render :new
+    puts @commission = Commission.new(	card: params[:commission][:card],
+    							 								deposit: params[:commission][:deposit]
+    														)
+    case params[:commission][:type] 
+    when "0"
+    	@commission.booking_id = params[:commission][:id]
+    when "1"
+    	@commission.event_id 	= params[:commission][:id]
+    	if Event.exists?(params[:commission][:id])
+    		event = Event.find(params[:commission][:id])
+    		event.hasCommission = true
+    		puts '************+'
+    		puts event.save
+    	end
+    else
+    	@commission.user_id = params[:commission][:id]
+    end
+
+    if @commission.save
+      flash[:success] = "Commission successfully saved"
+    else
+      flash[:error] = "Commission not saved in database"
+    end
   end
 end
