c940e1505a0b2646dcbf445a0591e0460bf5b548
Vanessa <van.gg@live.com.mx>
2017-01-27 08:01:20 -0600

    Config wicked_pdf to work in production

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -7,15 +7,22 @@
 # To learn more, check out the README:
 #
 # https://github.com/mileszs/wicked_pdf/blob/master/README.md
+if Rails.env.production?
+    wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf-amd64"
+else
+    wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf"
+end
 
 WickedPdf.config = {
-  # Path to the wkhtmltopdf executable: This usually isn't needed if using
-  # one of the wkhtmltopdf-binary family of gems.
-  exe_path: '/usr/local/bin/wkhtmltopdf',
-  #   or
-  # exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
 
-  # Layout file to be used for all PDFs
-  # (but can be overridden in `render :pdf` calls)
-  # layout: 'pdf.html',
-}
+      :exe_path     => wkhtmltopdf_path,
+      :wkhtmltopdf  => wkhtmltopdf_path,
+      :orientation  => 'Portrait',
+      :layout       => "pdf.html",
+      :footer       => {
+          :left     => "Left",
+          :right    => "#{Time.now}",
+          :font_size => 5,
+          :center   => '[page] de [topage]'
+      }
+  }
