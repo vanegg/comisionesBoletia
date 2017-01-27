9c26f913447ee1c0ed837de24184854519ef008c
Vanessa <van.gg@live.com.mx>
2017-01-27 07:14:18 -0600

    Increase number of users in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-3.times do |n|
+10.times do |n|
 	first_name 	= Faker::Name.first_name
 	last_name		= Faker::Name.last_name
 	user = User.create!(	name: 		first_name + ' ' + last_name,
